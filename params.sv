86ffcd626b8b9a3258692074140f790d5effd4e5
Dav-11 <48259205+Dav-11@users.noreply.github.com>
2025-06-08 11:10:07 +0200

    cache controller I/O

--- params.sv
+++
@@ -1,8 +0,0 @@
-package params_pkg;
-    parameter ADDR_WIDTH = 32;
-    parameter DATA_WIDTH = 32;
-    parameter ID_WIDTH = 4;
-    parameter USER_WIDTH = 0;
-    parameter SNOOP_DATA_BUS_WIDTH = 1;
-    parameter SNOOP_ADD_BUS_WIDTH = 1;
-endpackage : params_pkg
