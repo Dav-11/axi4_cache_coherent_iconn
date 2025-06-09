18412573cc560d38eb920537feffc20c77a791bd
Dav-11 <48259205+Dav-11@users.noreply.github.com>
2025-06-09 15:55:31 +0200

    stylw

--- hdl/cache_controller.sv
+++ hdl/cache_controller.sv
@@ -7,23 +7,23 @@ module cache_controller (
     // -----------------------------
     //  AW (Write Address) Channel
     // -----------------------------
-    output logic          [  ID_WIDTH-1:0] aw_id,
-    output logic          [ADDR_WIDTH-1:0] aw_addr,
-    output logic          [           7:0] aw_len,
-    output axi_burst_size                  aw_size,
-    output axi_burst                       aw_burst,
-    output axi_lock                        aw_lock,
-    output axi_aw_cache                    aw_cache,
-    output axi_prot                        aw_prot,
-    output logic          [           3:0] aw_qos,
-    output logic          [           3:0] aw_region,
-    output logic          [USER_WIDTH-1:0] aw_user,
-    output logic                           aw_valid,
-    input  logic                           aw_ready,
-    output logic          [           1:0] aw_domain,
-    output logic          [           2:0] aw_snoop,
-    output logic          [           1:0] aw_bar,
-    output logic                           aw_unique,
+    output logic            [  ID_WIDTH-1:0] aw_id,
+    output logic            [ADDR_WIDTH-1:0] aw_addr,
+    output logic            [           7:0] aw_len,
+    output axi_burst_size_e                  aw_size,
+    output axi_burst_e                       aw_burst,
+    output axi_lock_e                        aw_lock,
+    output axi_aw_cache_e                    aw_cache,
+    output axi_prot_e                        aw_prot,
+    output logic            [           3:0] aw_qos,
+    output logic            [           3:0] aw_region,
+    output logic            [USER_WIDTH-1:0] aw_user,
+    output logic                             aw_valid,
+    input  logic                             aw_ready,
+    output logic            [           1:0] aw_domain,
+    output logic            [           2:0] aw_snoop,
+    output logic            [           1:0] aw_bar,
+    output logic                             aw_unique,
 
     // -----------------------------
     //  W (Write Data) Channel
@@ -39,52 +39,52 @@ module cache_controller (
     // -----------------------------
     //  B (Write Response) Channel
     // -----------------------------
-    input  logic    [  ID_WIDTH-1:0] b_id,
-    input  axi_resp                  b_resp,
-    input  logic    [USER_WIDTH-1:0] b_user,
-    input  logic                     b_valid,
-    output logic                     b_ready,
+    input  logic      [  ID_WIDTH-1:0] b_id,
+    input  axi_resp_e                  b_resp,
+    input  logic      [USER_WIDTH-1:0] b_user,
+    input  logic                       b_valid,
+    output logic                       b_ready,
 
     // -----------------------------
     //  AR (Address Read) Channel
     // -----------------------------
-    output logic          [  ID_WIDTH-1:0] ar_id,
-    output logic          [ADDR_WIDTH-1:0] ar_addr,
-    output logic          [           7:0] ar_len,
-    output axi_burst_size                  ar_size,
-    output axi_burst                       ar_burst,
-    output axi_lock                        ar_lock,
-    output axi_ar_cache                    ar_cache,
-    output axi_prot                        ar_prot,
-    output logic          [           3:0] ar_qos,
-    output logic          [           3:0] ar_region,
-    output logic          [USER_WIDTH-1:0] ar_user,
-    output logic                           ar_valid,
-    input  logic                           ar_ready,
-    output logic          [           1:0] ar_domain,
-    output logic          [           3:0] ar_snoop,
-    output logic          [           1:0] ar_bar,
+    output logic            [  ID_WIDTH-1:0] ar_id,
+    output logic            [ADDR_WIDTH-1:0] ar_addr,
+    output logic            [           7:0] ar_len,
+    output axi_burst_size_e                  ar_size,
+    output axi_burst_e                       ar_burst,
+    output axi_lock_e                        ar_lock,
+    output axi_ar_cache_e                    ar_cache,
+    output axi_prot_e                        ar_prot,
+    output logic            [           3:0] ar_qos,
+    output logic            [           3:0] ar_region,
+    output logic            [USER_WIDTH-1:0] ar_user,
+    output logic                             ar_valid,
+    input  logic                             ar_ready,
+    output logic            [           1:0] ar_domain,
+    output logic            [           3:0] ar_snoop,
+    output logic            [           1:0] ar_bar,
 
     // -----------------------------
     //  R (Data Read) Channel
     // -----------------------------
-    input  logic    [  ID_WIDTH-1:0] r_id,
-    input  logic    [DATA_WIDTH-1:0] r_data,
-    input  axi_resp                  r_resp_axi,
-    input  logic    [           1:0] r_resp_ace,
-    input  logic                     r_last,
-    input  logic    [USER_WIDTH-1:0] r_user,
-    input  logic                     r_valid,
-    output logic                     r_ready,
+    input  logic      [  ID_WIDTH-1:0] r_id,
+    input  logic      [DATA_WIDTH-1:0] r_data,
+    input  axi_resp_e                  r_resp_axi,
+    input  logic      [           1:0] r_resp_ace,
+    input  logic                       r_last,
+    input  logic      [USER_WIDTH-1:0] r_user,
+    input  logic                       r_valid,
+    output logic                       r_ready,
 
     // -----------------------------
     //  AC (Snoop Address) Channel
     // -----------------------------
-    input  logic                                    ac_valid,
-    output logic                                    ac_ready,
-    input  logic        [SNOOP_ADD_BUS_WIDTH - 1:0] ac_addr,
-    input  ace_ac_snoop                             ac_snoop,
-    input  logic        [                      2:0] ac_prot,
+    input  logic                                      ac_valid,
+    output logic                                      ac_ready,
+    input  logic          [SNOOP_ADD_BUS_WIDTH - 1:0] ac_addr,
+    input  ace_ac_snoop_e                             ac_snoop,
+    input  logic          [                      2:0] ac_prot,
 
     // -----------------------------
     //  CR (Snoop Response) Channel
--- hdl/types.sv
+++ hdl/types.sv
@@ -3,97 +3,97 @@
 // -------------------------
 
 typedef enum logic [1:0] {
-    FIXED       = 2'b00,
-    INCR        = 2'b01,
-    WRAP        = 2'b10,
-    RESERVED    = 2'b11
-} axi_burst;
+    FIXED    = 2'b00,
+    INCR     = 2'b01,
+    WRAP     = 2'b10,
+    RESERVED = 2'b11
+} axi_burst_e;
 
 typedef enum logic {
-    NORMAL      = 1'b0,
-    EXCLUSIVE   = 1'b1
-} axi_lock;
+    NORMAL    = 1'b0,
+    EXCLUSIVE = 1'b1
+} axi_lock_e;
 
 typedef enum logic [1:0] {
-    OKAY        = 2'b00,
-    EXOKAY      = 2'b01,
-    SLVERR      = 2'b10,
-    DECERR      = 2'b11
-} axi_resp;
+    OKAY   = 2'b00,
+    EXOKAY = 2'b01,
+    SLVERR = 2'b10,
+    DECERR = 2'b11
+} axi_resp_e;
 
 typedef enum logic [2:0] {
-    S_001       = 3'b000,
-    S_002       = 3'b001,
-    S_004       = 3'b010,
-    S_008       = 3'b011,
-    S_016       = 3'b100,
-    S_032       = 3'b101,
-    S_064       = 3'b110,
-    S_128       = 3'b111
-} axi_burst_size;
+    S_001 = 3'b000,
+    S_002 = 3'b001,
+    S_004 = 3'b010,
+    S_008 = 3'b011,
+    S_016 = 3'b100,
+    S_032 = 3'b101,
+    S_064 = 3'b110,
+    S_128 = 3'b111
+} axi_burst_size_e;
 
-typedef enum logic[3:0] {
-    DEV_NON_BUFFERABLE_AW                  = 4'b0000,
-    DEV_BUFFERABLE_AW                      = 4'b0001,
+typedef enum logic [3:0] {
+    DEV_NON_BUFFERABLE_AW = 4'b0000,
+    DEV_BUFFERABLE_AW     = 4'b0001,
 
-    NORMAL_NC_NBUF_AW                      = 4'b0010, // Normal Non-cacheable Non-bufferable
-    NORMAL_NC_BUF_AW                       = 4'b0011, // Normal Non-cacheable Bufferable
+    NORMAL_NC_NBUF_AW = 4'b0010,  // Normal Non-cacheable Non-bufferable
+    NORMAL_NC_BUF_AW  = 4'b0011,  // Normal Non-cacheable Bufferable
 
-    WRITE_THROUGH_NO_ALLOCATE_AW           = 4'b0110,
-    WRITE_THROUGH_READ_ALLOCATE_AW         = 4'b0110,
-    WRITE_THROUGH_WRITE_ALLOCATE_AW        = 4'b1110,
-    WRITE_THROUGH_READ_WRITE_ALLOCATE_AW   = 4'b1110,
+    WRITE_THROUGH_NO_ALLOCATE_AW         = 4'b0110,
+    WRITE_THROUGH_READ_ALLOCATE_AW       = 4'b0110,
+    WRITE_THROUGH_WRITE_ALLOCATE_AW      = 4'b1110,
+    WRITE_THROUGH_READ_WRITE_ALLOCATE_AW = 4'b1110,
 
-    WRITE_BACK_NO_ALLOCATE_AW              = 4'b0111,
-    WRITE_BACK_READ_ALLOCATE_AW            = 4'b0111,
-    WRITE_BACK_WRITE_ALLOCATE_AW           = 4'b1111,
-    WRITE_BACK_READ_WRITE_ALLOCATE_AW      = 4'b1111
-} axi_aw_cache;
+    WRITE_BACK_NO_ALLOCATE_AW         = 4'b0111,
+    WRITE_BACK_READ_ALLOCATE_AW       = 4'b0111,
+    WRITE_BACK_WRITE_ALLOCATE_AW      = 4'b1111,
+    WRITE_BACK_READ_WRITE_ALLOCATE_AW = 4'b1111
+} axi_aw_cache_e;
 
-typedef enum logic[3:0] {
-    DEV_NON_BUFFERABLE_AR                  = 4'b0000,
-    DEV_BUFFERABLE_AR                      = 4'b0001,
+typedef enum logic [3:0] {
+    DEV_NON_BUFFERABLE_AR = 4'b0000,
+    DEV_BUFFERABLE_AR     = 4'b0001,
 
-    NORMAL_NC_NBUF_AR                      = 4'b0010, // Normal Non-cacheable Non-bufferable
-    NORMAL_NC_BUF_AR                       = 4'b0011, // Normal Non-cacheable Bufferable
+    NORMAL_NC_NBUF_AR = 4'b0010,  // Normal Non-cacheable Non-bufferable
+    NORMAL_NC_BUF_AR  = 4'b0011,  // Normal Non-cacheable Bufferable
 
-    WRITE_THROUGH_NO_ALLOCATE_AR           = 4'b1010,
-    WRITE_THROUGH_READ_ALLOCATE_AR         = 4'b1110,
-    WRITE_THROUGH_WRITE_ALLOCATE_AR        = 4'b1010,
-    WRITE_THROUGH_READ_WRITE_ALLOCATE_AR   = 4'b1110,
+    WRITE_THROUGH_NO_ALLOCATE_AR         = 4'b1010,
+    WRITE_THROUGH_READ_ALLOCATE_AR       = 4'b1110,
+    WRITE_THROUGH_WRITE_ALLOCATE_AR      = 4'b1010,
+    WRITE_THROUGH_READ_WRITE_ALLOCATE_AR = 4'b1110,
 
-    WRITE_BACK_NO_ALLOCATE_AR              = 4'b1011,
-    WRITE_BACK_READ_ALLOCATE_AR            = 4'b1111,
-    WRITE_BACK_WRITE_ALLOCATE_AR           = 4'b1011,
-    WRITE_BACK_READ_WRITE_ALLOCATE_AR      = 4'b1111
-} axi_ar_cache;
+    WRITE_BACK_NO_ALLOCATE_AR         = 4'b1011,
+    WRITE_BACK_READ_ALLOCATE_AR       = 4'b1111,
+    WRITE_BACK_WRITE_ALLOCATE_AR      = 4'b1011,
+    WRITE_BACK_READ_WRITE_ALLOCATE_AR = 4'b1111
+} axi_ar_cache_e;
 
 // Bit [2] is Privileged/Unprivileged
 // Bit [1] is Secure/Non-secure
 // Bit [0] is Instruction/Data
 typedef enum logic [2:0] {
-    PROT_UNPRIVILEGED_NON_SECURE_DATA   = 3'b000, // Unprivileged, Non-secure, Data
-    PROT_UNPRIVILEGED_NON_SECURE_INSTR  = 3'b001, // Unprivileged, Non-secure, Instruction
+    PROT_UNPRIVILEGED_NON_SECURE_DATA  = 3'b000,  // Unprivileged, Non-secure, Data
+    PROT_UNPRIVILEGED_NON_SECURE_INSTR = 3'b001,  // Unprivileged, Non-secure, Instruction
 
-    PROT_UNPRIVILEGED_SECURE_DATA       = 3'b010, // Unprivileged, Secure, Data
-    PROT_UNPRIVILEGED_SECURE_INSTR      = 3'b011, // Unprivileged, Secure, Instruction
+    PROT_UNPRIVILEGED_SECURE_DATA  = 3'b010,  // Unprivileged, Secure, Data
+    PROT_UNPRIVILEGED_SECURE_INSTR = 3'b011,  // Unprivileged, Secure, Instruction
 
-    PROT_PRIVILEGED_NON_SECURE_DATA     = 3'b100, // Privileged, Non-secure, Data
-    PROT_PRIVILEGED_NON_SECURE_INSTR    = 3'b101, // Privileged, Non-secure, Instruction
+    PROT_PRIVILEGED_NON_SECURE_DATA  = 3'b100,  // Privileged, Non-secure, Data
+    PROT_PRIVILEGED_NON_SECURE_INSTR = 3'b101,  // Privileged, Non-secure, Instruction
 
-    PROT_PRIVILEGED_SECURE_DATA         = 3'b110, // Privileged, Secure, Data
-    PROT_PRIVILEGED_SECURE_INSTR        = 3'b111  // Privileged, Secure, Instruction
-} axi_prot;
+    PROT_PRIVILEGED_SECURE_DATA  = 3'b110,  // Privileged, Secure, Data
+    PROT_PRIVILEGED_SECURE_INSTR = 3'b111   // Privileged, Secure, Instruction
+} axi_prot_e;
 
-typedef enum logic  [3:0] {
-    READ_ONCE               = 4'b0000,
-    READ_SHARED             = 4'b0001,
-    READ_CLEAN              = 4'b0010,
-    READ_NOT_SHARED_DIRTY   = 4'b0011,
-    READ_UNIQUE             = 4'b0111,
-    CLEAN_SHARED            = 4'b1000,
-    CLEAN_INVALID           = 4'b1001,
-    MAKE_INVALID            = 4'b1101,
-    DVM_COMPLETE            = 4'b1110,
-    DVM_MESSAGE             = 4'b1111
-} ace_ac_snoop;
+typedef enum logic [3:0] {
+    READ_ONCE             = 4'b0000,
+    READ_SHARED           = 4'b0001,
+    READ_CLEAN            = 4'b0010,
+    READ_NOT_SHARED_DIRTY = 4'b0011,
+    READ_UNIQUE           = 4'b0111,
+    CLEAN_SHARED          = 4'b1000,
+    CLEAN_INVALID         = 4'b1001,
+    MAKE_INVALID          = 4'b1101,
+    DVM_COMPLETE          = 4'b1110,
+    DVM_MESSAGE           = 4'b1111
+} ace_ac_snoop_e;
